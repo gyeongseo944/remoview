5cd0f08411989cd6c9cfa0015d251c9a3a3f8a0f
DESKTOP-8H3R7HO\gyeon <gyeongseo944@gmail.com>
2022-02-23 16:29:22 +0900

    move private information into dev.js

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
-mongoose.connect('mongodb://Gyeongseo:gyeongseo123@remoview-shard-00-00.xmvyq.mongodb.net:27017,remoview-shard-00-01.xmvyq.mongodb.net:27017,remoview-shard-00-02.xmvyq.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-yv9mog-shard-0&authSource=admin&retryWrites=true&w=majority',
+const config = require('./config/keys')
+
+const { User } = require('./models/user');
+
+
+mongoose.connect(config.mongoURI,
     {}).then(()=>{console.log('DB CONNECTED')})
-                            .catch(err=>{console.log('===DB error=== ::: '+err)})
+        .catch(err=>{console.log('===DB error=== ::: '+err)}
+);
 app.get('/',(req,res)=>{
-    res.send('hello world!')
+    res.send('is it works? yeahhhh  it works!')
 });
 
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post('/api/users/register',(req,res)=>{
+    const user = new User(req.body)
+    user.save((err,userData)=>{
+        if(err) return res.json({success:false, err})
+    })
+    return res.status(200).json({
+        success:true
+    })
+})
 
 app.listen(5000);
 
