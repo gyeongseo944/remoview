7652e1fec41ce8848b122f11419570e4436ea7ee
DESKTOP-8H3R7HO\gyeon <gyeongseo944@gmail.com>
2022-05-01 00:26:04 +0900

    test heroku

--- server/index.js
+++ server/index.js
@@ -25,10 +25,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
-
 app.use("/api/users", require("./routes/users"));
 app.use("/api/thumbs", require("./routes/thumbs"));
 app.use("/api/comment", require("./routes/comment"));
